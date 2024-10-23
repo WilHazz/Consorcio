dadf9b11e9cdb73f66c8f3e875156eef6577f3ab
Hanny Paz <hwpazz@gmail.com>
2024-10-23 13:12:02 -0500

    Start project

---
+++ src/main/java/consorcio/consorcio_project/ConsorcioProjectApplication.java
@@ -0,0 +1,13 @@
+package consorcio.consorcio_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsorcioProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsorcioProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/consorcio/consorcio_project/ConsorcioProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package consorcio.consorcio_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsorcioProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
